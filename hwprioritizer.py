4622a541c2a61e24474614c79779985d8b607bcf
Joshua Liou <116848053+joshualiou07@users.noreply.github.com>
2026-04-19 00:54:01 -0500

    Add files via upload

--- hwprioritizer.py
+++ hwprioritizer.py
@@ -8,6 +8,22 @@ class Prioritizer:
         self.assignments.append(hw)
 
     def sort(self):
+        sorted_list = []
+        copy = self.assignments[:]
+
+        while len(copy) > 0:
+            top_index = 0
+            highest_score = copy[0].score()
+
+            for i in range(1, len(copy)):
+                if copy[i].score() > highest_score:
+                    highest_score = copy[i].score()
+                    top_index = i
+
+            sorted_list.append(copy[top_index])
+            copy.pop(top_index)
+        
+        self.assignments = sorted_list
 
     def remove(self, index):
         if index >= 0 and index < len(self.assignments):
