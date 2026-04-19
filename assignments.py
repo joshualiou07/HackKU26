bfba99e4eb48d95737bbf1c24d5468fb0fcb88bb
Joshua Liou <116848053+joshualiou07@users.noreply.github.com>
2026-04-19 00:23:00 -0500

    Add files via upload

--- assignments.py
+++ assignments.py
@@ -3,3 +3,8 @@ class Assignments:
         self.name = name
         self.name = due
         self.name = difficulty
+
+    def points(self):
+        due_date = 1/(self.due + 1)
+        diff = self.difficulty/5
+        return due_date + diff
--- hwprioritizer.py
+++ hwprioritizer.py
@@ -4,6 +4,8 @@ class Prioritizer:
 
  #Ranked by point system based on difficulty and due date
     def add(self, days, difficulty):
+        hw = Assignments(days, difficulty)
+        self.assignments.append(hw)
 
     def sort(self):
 
