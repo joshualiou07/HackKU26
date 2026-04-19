055c01f776a75d9359f47c9ce6b2b6a82ea8c043
Joshua Liou <116848053+joshualiou07@users.noreply.github.com>
2026-04-19 00:02:34 -0500

    Add files via upload

---
+++ hwprioritizer.py
@@ -0,0 +1,10 @@
+class Prioritizer:
+    def __init__(self):
+        self.assignments = []
+
+ #Ranked by point system based on difficulty and due date
+    def add(self, days, difficulty):
+
+    def sort(self):
+
+    def remove(self):
